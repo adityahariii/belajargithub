8e01163c2544d9d2b79e49880b35fd5107b722e0
didithari <adityaqaharii@gmail.com>
2023-09-01 09:53:42 +0700

    belajar

---
+++ app/src/main/java/com/example/belajargit/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.belajargit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView txtName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtName = findViewById(R.id.txt_name);
+        txtName.setText("Swifties");
+
+    }
+
+}
